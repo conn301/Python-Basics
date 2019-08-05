4f6cc018774bb3b4c33c8582df83d2bb37c87b1b
Jared <48038146+conn301@users.noreply.github.com>
2019-08-04 22:28:46 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,24 @@
+#Supply the input for the creation of a short story.
+
+print("**********************")
+print("* Mad Libs Generator *")
+print("**********************")
+
+
+object1 = input("Object: ")
+object2 = input("Object: ")
+music = input("Music Artist: ")
+name = input("Friend's Name: ")
+scary = input("Scary Thing: ")
+
+
+print("I walked inside my house and grabbed the " + object1 + "." + " Then I went upstairs and found a " + object2 + ".")
+print("I wasn't sure what I wanted do with these things so I threw them away.")
+print("I went to my car and started playing music by " + music + ". Eh, I'm not really feeling this song.")
+print("I grabbed by phone and called " + name + ". " + name +" answered the phone so I asked if he/she wanted to hang out. ")
+print("I drove to his/her house and walked towards the front door. The door opened and I screamed!")
+print("I saw " + scary)
+
+
+
+
